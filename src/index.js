464e7ef937e38dc1fba07442707860678c884563
Anastasia <navarichanastasia@gmai.com>
2020-02-21 15:52:50 +0300

    complete the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(!array || array.length < 1){
+        return 0;
+    }
+    if(array.length < 1){
+        return 0;
+    }
+    for(let i = 0; i < array.length; i++){
+        array.sort((a, b) => b - a);
+        if(i === array.length - 1){
+            return array[i];
+        }
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(!array || array.length < 1){
+        return 0;
+    }
+    for(let i = 0; i < array.length; i++){
+        array.sort((a, b) => b - a);
+    }
+    return array[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(!array || array.length < 1){
+        return 0;
+    }
+    let a = 0;
+    for(let i = 0; i < array.length; i++){
+       a = a + parseInt(array[i]);
+    } 
+    a = a / array.length ;
+return a;
 }
